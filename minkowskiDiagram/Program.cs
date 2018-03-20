475a14c7c70581677837caaab126b56a66e74b9f
ftwdjw <ftwdjw@gmail.com>
2018-03-20 08:17:53 -0500

    works

--- minkowskiDiagram/Program.cs
+++ minkowskiDiagram/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
 
 namespace minkowskiDiagram
 {
     class MainClass
     {
+        public const int N = 10;
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i;
+            double x, t;
+
+            string[] minkowski = new string[N+1];
+            for (i = 0; i <= N; i++){
+                x = (double)i / (double)N;
+                t = x;
+                
+                minkowski[i] = String.Format("{0:N},{1:N}",x,t);
+            }
+            File.WriteAllLines("minkowski.csv",minkowski);
+
+            Console.WriteLine("have written file");
         }
     }
 }
