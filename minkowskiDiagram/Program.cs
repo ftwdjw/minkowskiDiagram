19cd3bdf2650c5bc36e216dd7f7a9ca213c391dc
ftwdjw <ftwdjw@gmail.com>
2018-03-20 14:26:06 -0500

    works

--- minkowskiDiagram/Program.cs
+++ minkowskiDiagram/Program.cs
@@ -9,14 +9,18 @@ namespace minkowskiDiagram
         public static void Main(string[] args)
         {
             int i;
-            double x, t;
+            double x, t, t1;
 
-            string[] minkowski = new string[N+1];
+            string[] minkowski = new string[N+2];
+
+            minkowski[0] = String.Format("light-sec. distance,sec. time");
+
             for (i = 0; i <= N; i++){
                 x = (double)i / (double)N;
                 t = x;
-                
-                minkowski[i] = String.Format("{0:N},{1:N}",x,t);
+                t1 = x * 1.11;
+
+                minkowski[i+1] = String.Format("{0:N},{1:N},{2:N}",x,t,t1);
             }
             File.WriteAllLines("minkowski.csv",minkowski);
 
